529885e737c483857533654567f339434e5c1061
njtucker0 <njtucker0@gmail.com>
2023-08-17 13:27:33 -0400

    initial header components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import { useState } from 'react';
+
+const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+};
 
 function App() {
+  const [word,setWord] = useState('');
+
   return (
     <div className="App">
       <Header title='Images Gallery'/>
-      <Search />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
+
+const navbarStyle = {
+    backgroundColor: 'lightblue'
+};
 
 const Header = () => {
     return (
-        <Navbar bg='light' variant='light'>
-            <Navbar.Brand href='/'>Images Gallery</Navbar.Brand>
+        <Navbar style={navbarStyle} variant='light'>
+            <Container>
+                <Navbar.Brand href='/'>Images Gallery</Navbar.Brand>
+            </Container>
         </Navbar>
     )
 };
