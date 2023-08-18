7d38c214dfe676667d72ff3f9c2b3699ec0ab0c4
njtucker0 <njtucker0@gmail.com>
2023-08-18 08:34:12 -0400

    initial header components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,24 @@ import Header from './components/Header';
 import Search from './components/Search';
 import { useState } from 'react';
 
-function App() {
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
+const App = () => {
   const [word,setWord] = useState('');
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-  };
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+  }
+
 
   return (
     <div className="App">
@@ -17,6 +28,6 @@ function App() {
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
-}
+};
 
 export default App;
