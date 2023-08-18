f28c09cee393432e3660ade5963be7f5accff7e9
njtucker0 <njtucker0@gmail.com>
2023-08-18 12:50:34 -0400

    ImageCard with deletion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
 import { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 const App = () => {
   const [word,setWord] = useState('');
+  const [images,setImages] = useState([]);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -14,7 +17,8 @@ const App = () => {
     fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
         .then((res) => res.json())
         .then((data) => {
-            console.log(data);
+            setImages([{...data,title:word},...images]);
+            console.log(images);
         })
         .catch((err) => {
             console.log(err);
@@ -22,10 +26,24 @@ const App = () => {
     setWord('');
   };
 
+  const handleDeleteImage = (id) => {
+    setImages(images.filter((image) => image.id !== id));
+  };
+
   return (
     <div className="App">
       <Header title='Images Gallery'/>
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
+      <Container className="mt-4">
+        <Row xs={1} md={2} lg={3}>
+          { images.map((image,i) => (
+            <Col key={i} className='pb-3'>
+              <ImageCard image={image} deleteImage={handleDeleteImage}/>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+
     </div>
   );
 };
