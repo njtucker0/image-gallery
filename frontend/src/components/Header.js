25f5f034fff2e000a3faef6dc937fa643fc48ae2
njtucker0 <njtucker0@gmail.com>
2023-08-18 13:28:57 -0400

    Welcome component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,11 +6,11 @@ const navbarStyle = {
     backgroundColor: '#eeeeee'
 };
 
-const Header = () => {
+const Header = ({title}) => {
     return (
         <Navbar style={navbarStyle} variant='light'>
             <Container>
-                <Logo style={{ maxWidth: '12rem',maxHeight: '2rem'}}/>
+                <Logo alt={title} style={{ maxWidth: '12rem',maxHeight: '2rem'}}/>
             </Container>
         </Navbar>
     )
---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const Welcome = () => (
+    <div className='jumbotron'>
+        <h1>Images Gallery</h1>
+        <p>
+          This is a simple application that retrieves photos using Unsplash API.
+          In order to start, enter any search term in the input field.
+        </p>
+        <p>
+          <Button variant='primary' href='https://unsplash.com'
+          target='_blank'>Learn more</Button>
+        </p>
+    </div>
+);
+
+export default Welcome;
