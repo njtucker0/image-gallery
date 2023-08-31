c2af47d6b3fa03e6a6c214da411d40e2c743f7c9
njtucker0 <njtucker0@gmail.com>
2023-08-31 11:08:50 -0400

    api backend components

--- api/app.py
+++ api/app.py
@@ -1,10 +1,29 @@
 from flask import Flask, request
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='./.env.local')
+
+UNSPLASH_URL = 'https://api.unsplash.com/photos/random'
+UNSPLASH_KEY = os.environ.get('UNSPLASH_KEY', '')
+
+if not UNSPLASH_KEY:
+    raise EnvironmentError("Please create .env.local file "
+                           "and insert the UNSPLASH_KEY")
 
 app = Flask(__name__)
 @app.route('/new-image')
 def new_image():
-    word = request.args.get('query')
-    return {"word": word}
+    headers = {
+        'Accept-Version': 'v1',
+        'Authorization': "Client-ID " + UNSPLASH_KEY
+    }
+    params = {'query': request.args.get('query')}
+    response = requests.get(url=UNSPLASH_URL,
+                            headers=headers,
+                            params=params)
+    return response.json()
 
 
 if __name__ == "__main__":
